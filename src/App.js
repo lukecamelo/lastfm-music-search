55801f41c52a68ec04f8375764a20f27ed9c86b2
luke <azathothbeats@gmail.com>
2018-04-21 20:16:11 -0400

    Search function in progress

--- src/App.js
+++ src/App.js
@@ -5,7 +5,10 @@ class App extends Component {
   constructor(props) {
     super(props);
 
+    this.searchQuery = React.createRef();
+
     this.state = {
+      apiCall: 'http://ws.audioscrobbler.com/2.0/?method=artist.gettopalbums&artist=prince&api_key=94815164058d345ac89d834b6c7c69c2&format=json',
       error: null,
       isLoaded: false,
       artistAlbums: []
@@ -13,7 +16,7 @@ class App extends Component {
   }
 
   componentDidMount() {
-    fetch(`http://ws.audioscrobbler.com/2.0/?method=artist.gettopalbums&artist=cher&api_key=94815164058d345ac89d834b6c7c69c2&format=json`)
+    fetch(this.state.apiCall)
       .then(res => res.json())
       .then(
         (result) => {
@@ -31,23 +34,43 @@ class App extends Component {
       )
   }
 
+  artistSearch(event) {
+    event.preventDefault();
+    console.log('you searched!');
+    const query = this.searchQuery.value;
+    this.setState({
+      apiCall: `http://ws.audioscrobbler.com/2.0/?method=artist.gettopalbums&artist=${query}&api_key=94815164058d345ac89d834b6c7c69c2&format=json`
+    });
+  }
+
   render() {
+
     const {isLoaded, artistAlbums, error} = this.state;
+
     if (error) {
       return <div>Error: {error.message}</div>;
-    } else if (!isLoaded) {
+    }
+     
+    else if (!isLoaded) {
       return <div>Loading...</div>
-    } else {
+    }
+     
+    else {
       return (
-        <ul>
-          {artistAlbums.map(album => (
-            <li key={album.name}>
-              {album.name} <br/>
-              {album.playcount} <br/>
-              {album.artist.name} <br/>
-            </li>
-          ))}
-        </ul>
+        <div>
+          <form action="" onSubmit={(e) => this.artistSearch(e)}>
+            <input type="text" ref={this.searchQuery} />
+          </form>
+          <ul>
+            {artistAlbums.map(album => (
+              <li key={album.name}>
+                <img src={`${album.image[2]['#text']}`} alt='album'/> <br/>
+                {album.playcount} <br/>
+                {album.artist.name} <br/>
+              </li>
+            ))}
+          </ul>
+        </div>
       );
     }
     // return (
