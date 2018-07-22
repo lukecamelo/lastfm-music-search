bb9439e301a4f4111d4e388377bd72f7386deedf
Luke Camelo <azathothbeats@gmail.com>
2018-07-22 18:25:47 -0400

    Added proper readme, other misc changes

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-This is a web app that allows you to look up artists via Last.fm's REST API! See summaries of the artist's background and their most popular albums and songs. Built with ReactJS.
+# Last.fm Music Search
+
+### What's this?
+It's a web application that allows you to look up bands/artists and get a short little bio about them, as well as album covers for their top 3 albums, and their respective playcounts. Just a fun little project I wrote for practice.
+### What's it made of?
+I built it in react as an exercise in, well, react! I wanted to build a smallish project to wrap my head around the framework.
+### Neat, how do I run it?
+After cloning the project, `cd` into the directory and enter `npm start` in your console or terminal. You're up and running!
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,22 +1,25 @@
-import React from 'react';
-import './SearchBar.css';
+import React from 'react'
+import './SearchBar.css'
 
-const SearchBar = (props) => {
+const SearchBar = props => {
   return (
-    <div className='SearchForm'>
-      <form action="" onSubmit={props.submit} className='searchBar'>
-
-        <input type="text" 
-        value={props.query} 
-        onChange={props.change} 
-        placeholder='Artist search'
-        className='searchInput' />
-
-        <button className='albumButton' onClick={props.click}>Show albums!</button>
-        
+    <div className="SearchForm">
+      <form action="" onSubmit={props.submit} className="searchBar">
+        <div class="column">
+          <input
+            type="text"
+            value={props.query}
+            onChange={props.change}
+            placeholder="Artist search"
+            className="searchInput"
+          />
+          <button className="albumButton" onClick={props.click}>
+            Show albums!
+          </button>
+        </div>
       </form>
     </div>
   )
 }
 
-export default SearchBar;
+export default SearchBar
